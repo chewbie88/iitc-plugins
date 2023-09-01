fd258c9fc37d60efc8f221b1c5ee00de1194af43
chewbie88 <126526471+chewbie88@users.noreply.github.com>
2023-09-01 20:07:01 +0200

    Add files via upload

--- reims_inventory_sheet_helper.user.js
+++ reims_inventory_sheet_helper.user.js
@@ -5,6 +5,7 @@
 // @category       Info
 // @version        0.2.9
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://github.com/chewbie88/iitc-plugins/raw/main/reims_inventory_sheet_helper.meta.js
 // @downloadURL    https://github.com/chewbie88/iitc-plugins/raw/main/reims_inventory_sheet_helper.user.js
 // @description    Exports your inventory to the Inventroy form of the anomaly.
 // @include        https://intel.ingress.com/*
